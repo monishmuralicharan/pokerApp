4cca832f44e41f02346aa934c32795102d2ec0d3
monish237 <monish.muralicharan@gmail.com>
2024-08-26 16:20:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Database Connection
+mongoose.connect('mongodb://localhost:27017/mymernapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+// Serve the React app for any other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
