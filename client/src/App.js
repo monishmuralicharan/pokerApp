ea30c47095c390d29e70a03d093335f514d42cfa
monish237 <monish.muralicharan@gmail.com>
2024-08-26 16:48:08 -0400

    renamed pages 1, 2, 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Page1 from './components/Page1';
-import Page2 from './components/Page2';
+import Home from './components/Home';
+import ChipPage from './components/Chip-Page';
 import Page3 from './components/Page3';
-import Page4 from './components/Page4'; // Import the new component
+import ClosePage from './components/Close-Page'; // Import the new component
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/page1" element={<Page1 />} />
-        <Route path="/page2" element={<Page2 />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/chip-count" element={<ChipPage />} />
         <Route path="/page3" element={<Page3 />} />
-        <Route path="/page4" element={<Page4 />} /> {/* Add the new route */}
+        <Route path="/close-page" element={<ClosePage />} /> {/* Add the new route */}
       </Routes>
     </Router>
   );
